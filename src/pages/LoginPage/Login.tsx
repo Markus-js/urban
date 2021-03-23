76d2fbdd7f9012651b02a00c99ef16e4dc584225
Oliver Juul <Oliverjuul9@gmail.com>
2021-03-23 10:09:54 +0100

    .

---
+++ src/pages/LoginPage/Login.tsx
@@ -0,0 +1,17 @@
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/react'
+import React, { FC } from 'react'
+
+const Login: FC = () => (
+    <IonPage>
+        <IonHeader>
+            <IonToolbar>
+                <IonTitle>Login</IonTitle>
+            </IonToolbar>
+        </IonHeader>
+        <IonContent>
+            Stuff
+        </IonContent>
+    </IonPage>
+);
+
+export default Login
